5a89ca092ad3114cdb73639e3a515b3793207f94
hormozhalimi <halimihormoz@gmail.com>
2023-10-18 21:00:49 -0700

    PolyMorPhism

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+
 public class Main {
     public static void main(String[] args) {
-        Movie theMovie = new Adventure("Stars Wars");
-        theMovie.watchMovie();
+//        Movie theMovie = Movie.getMovie("Adventure", "star Wars");
+//        theMovie.watchMovie();
+
+        Scanner s = new Scanner(System.in);
+        while(true){
+            System.out.println("Enter Type (A for Adventure, C for Comedy, " + "S for Science Ficktion, or Q to quite");
+            String type =s.nextLine();
+            if ("Qq".contains(type)){
+                break;
+            }
+            System.out.println("Enter Movie Title ");
+            String title = s.nextLine();
+            Movie movie =  Movie.getMovie(type, title);
+            movie.watchMovie();
+
+        }
+
+
+
     }
 }
 
